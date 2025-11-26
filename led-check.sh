42978a5e6701341d1718c376aec3f8f4140c52cf
gargomoma <gargomoma@users.noreply.github.com>
2025-11-26 18:58:17 +0100

    Add files via upload
    based on
    https://github.com/LongQT-sea/OpenStick-Builder/issues/3

---
+++ led-check.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Check if wlan0 is UP
+if ip link show wlan0 | grep -q 'UP'; then
+    echo 1 > /sys/class/leds/green/brightness
+    echo 0 > /sys/class/leds/red/brightness
+else
+    echo 0 > /sys/class/leds/green/brightness
+    echo 1 > /sys/class/leds/red/brightness
+fi
+
+# Check if modem is connected
+if mmcli -m 0 | grep -q 'connected'; then
+    echo 1 > /sys/class/leds/blue/brightness
+else
+    echo 0 > /sys/class/leds/blue/brightness
+fi
